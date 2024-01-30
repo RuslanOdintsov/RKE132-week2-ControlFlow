8c8ea63132533c0c6a59ed7526637ef6b8ea9e9f
Ruslan.Odintsov <rodintso@tktk.ee>
2024-01-30 23:12:07 +0200

    create new app

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,32 @@
+﻿//Rakendus küsib kasutajal sisestada enda vanus
+//Kui kasutaja vanus on väiksem kui 18, siis konsoolis kuvatakse:
+//"You are too young to use tor browser"
+//Muul juhul
+//Konsoolis kuvatakse "welcome to tor browser"
+//int-number
+
+Console.WriteLine("You are entering Tor Browser. To use this browser you have to be at least 18 years old. Please enter your age:");
+string userAge = Console.ReadLine();
+int userAgeNum = 0;
+
+//int userAge = Int32.Parse(Console.ReadLine());  //"13"-heap 13-stack
+//boolean - true/false
+
+bool isAgeNumber = Int32.TryParse(userAge, out userAgeNum);
+Console.WriteLine($"Parse result {isAgeNumber}. User is {userAgeNum} years old.");
+
+if (isAgeNumber)
+{
+    if (userAgeNum >= 18)
+    {
+        Console.WriteLine("Welcome to Tor Browser");
+    }
+    else
+    {
+        Console.WriteLine("You don't have permission to use Tor Browser, because you are too young >:D");
+    }
+}
+else
+{
+    Console.WriteLine("Couldn't read your age");
+}
---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Hello, Master");
+//Rakendus küsib kasutajal valida enda sugu m/f
+//Rakendus küsib kasutajal sisestada enda perekonnanime
+//Lähtudes valikutest, rakendus tervitab kasutajat järgmiselt:
+//Welcome, Mr.(kasutaja perekonnanimi)/Welcome, Ms.(kasutaja perekonnanimi)
+
+Console.WriteLine("Please enter your gender here (m/f):");
+char userGender = Char.Parse(Console.ReadLine()); //loeb andmeid maha string formaadis
+Console.WriteLine("Please enter your lastname here:");
+string userLastname = Console.ReadLine();
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr.{userLastname}");
+}
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms.{userLastname}");
+}
+else
+{
+    Console.WriteLine($"Welcome, unknown gender {userLastname}");
+}
+//else
+//{
+//Console.WriteLine("Welcome")
+//}
